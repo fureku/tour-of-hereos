e6bffa24900e551ae17ed079a8d0ce4b7f356875
Fureku <goldsharkser@gmail.com>
2020-09-06 17:30:47 +0200

    last update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  heroes= HEROES;
+  heroes = HEROES;
+  selectedHero: Hero;
 
-
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    //this.selectedHero=this.heroes[0];
   }
 
+  ngOnInit() {}
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    /*console.log(this.selectedHero.name);*/
+  }
 }
